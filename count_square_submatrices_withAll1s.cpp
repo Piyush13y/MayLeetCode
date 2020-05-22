abc6b4e44145ac649dd31ceba4833b5766d76e0d
Piyush Yadav <piyush13y@gmail.com>
2020-05-22 23:43:38 +0530

    May 21st

--- count_square_submatrices_withAll1s.cpp
+++ count_square_submatrices_withAll1s.cpp
@@ -46,13 +46,12 @@ public:
         int ans = 0;
         for (int i = 0; i < matrix.size(); i++)
             for (int j = 0; j < matrix[0].size(); j++)
-                if (matrix[i][j])
+                if (matrix[i][j] == 1)
                 {
-                    if (i == 1 && j == 1)
+                    if (i != 0 && j != 0)
                         matrix[i][j] = min(matrix[i - 1][j], min(matrix[i - 1][j - 1], matrix[i][j - 1])) + 1;
                     ans += matrix[i][j];
                 }
         return ans;
     }
 };
-
